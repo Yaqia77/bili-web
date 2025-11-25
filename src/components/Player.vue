0e69b18ad259081140101ad02ab720ccfc5d1a84
yaqia77 <1456415343@qq.com>
2025-11-25 17:59:53 +0800

    feat: 优化视频播放器组件，增强弹幕功能和用户信息展示，提升视频详情页面体验

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,138 +1,295 @@
 <template>
-    <div class="player-panel">
-        <div class="player-style" ref="playerRef" :style="{height:playerHeight + 'px'}">
-            <div class="danmu-panel">
-                <div class="watcher">
-                    {{ onLineCount }} 人正在观看,已装填{{ danmuCount }}条弹幕
-                </div>
-                <div id="danmu" v-show="showDanmu"></div>
-                <div class="close-danmu" v-show="!showDanmu">已关闭弹幕</div>
-            </div>
-            <div id="play"><img :src="getLocalImage('play.png')" alt=""></div>
-        </div>
+  <div class="player-panel">
+    <div class="player-style" ref="playerRef" :style="{ height: playerHeight + 'px' }">
+      <div class="danmu-panel">
+        <div class="watcher">{{ onLineCount }} 人正在观看,已装填{{ danmuCount }}条弹幕</div>
+        <div id="danmu" v-show="showDanmu"></div>
+        <div class="close-danmu" v-show="!showDanmu">已关闭弹幕</div>
+      </div>
+      <div id="play">
+        <img :src="getLocalImage('play.png')" alt />
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted, watch, nextTick,onUnmounted } from "vue";
+import { ref, onMounted, watch, nextTick, onUnmounted, getCurrentInstance } from "vue";
 import { getLocalImage } from "@/utils/utils.js";
-import eventBus from "@/eventbus/eventBus.js";
+import { eventBus } from "@/eventbus/eventBus.js";
 import Hls from "hls.js";
 import Artplayer from "artplayer";
 import artplayerPluginDanmuku from "artplayer-plugin-danmuku";
 import { useLoginStore } from "@/stores/loginStore";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const { proxy } = getCurrentInstance();
 const loginStore = useLoginStore();
-const playerRef = ref()
+const playerRef = ref();
 
-let player = null
+let player = null;
+const initPlayer = () => {
+  //隐藏右键菜单
+  Artplayer.CONTEXTMENU = false;
+  //自动回放功能的最大记录数，默认为10
+  Artplayer.AUTO_PLAYBACK_MAX = 20;
+  //自动回放功能的最小记录时长，单位为秒，默认为5
+  Artplayer.AUTO_PLAYBACK_MIN = 10;
 
+  player = new Artplayer({
+    container: playerRef.value,
+    url: ``,
+    type: "m3u8",
+    customType: {
+      m3u8(video, url, art) {
+        if (Hls.isSupported()) {
+          if (art.hls) art.hls.destroy();
+          const hls = new Hls();
+          hls.loadSource(url);
+          hls.attachMedia(video);
+          art.hls = hls;
+          art.on("destroy", () => hls.destroy());
+        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+          video.src = url;
+        } else {
+          art.notice.show = "浏览器暂不支持该播放器";
+        }
+      }
+    },
+    theme: "#23ade5",
+    volume: 0.7, //默认音量
+    autoplay: true, //自动播放
+    loop: false, //循环播放
+    autoMini: false, //当播放器滚动到视口以外时，自动进入迷你播放器
+    fullscreen: true, //是否开启全屏
+    settings: [],
+    pip: true, //画中画
+    playbackRate: true, //播放速度
+    flip: true, //翻转
+    aspectRatio: true, //宽高比
+    screenshot: true, //截屏
+    autoPlayback: true, //自动回放
+    icons: {
+      state: document.querySelector("#play")
+    },
+    controls: [
+      {
+        name: "wide-screen",
+        position: "right",
+        html: '<span class="iconfont icon-wide-screen"></span>',
+        tooltip: "宽屏模式",
+        style: {
+          color: "#fff",
+          display: "flex"
+        },
+
+        click(...args) {
+          changeWideScreen();
+        }
+      },
+      {
+        name: "narrow-screen",
+        position: "right",
+        html: '<span class="iconfont icon-narrow-screen"></span>',
+        tooltip: "退出宽屏模式",
+        style: {
+          color: "#fff",
+          display: "none"
+        },
+        click(...args) {
+          changeWideScreen();
+        }
+      }
+    ],
+    plugins: [
+      artplayerPluginDanmuku({
+        mount: document.querySelector("#danmu"),
+        theme: "light",
+        emitter: true,
+        danmuku() {
+          return new Promise(async resolve => {
+            const danmuList = await loadDanmuList();
+            return resolve(danmuList);
+          })
+        },
+        beforeEmit: async (danmu) => {
+          let result = await postDanmu(danmu)
+          loadDanmuList();
+          if (!result) {
+            return false
+          }
+          eventBus.emit('danmSend')
+          proxy.message.success('弹幕发送成功')
+          return true
+        }
+      })
+    ],
+
+
+  });
+  player.on('hover', (state) => {
+    let display = 'none'
+    if (state) {
+      display = 'flex'
+    }
+    player.template.$bottom.style.display = display
+  })
+  player.on('video:ended', (e) => {
+    eventBus.emit("playEnd");
+  })
+}
+
+onMounted(() => {
+  nextTick(() => {
+    initPlayer();
+  })
+})
+
+const playerHeight = ref(500);
+
+const fileId = ref()
+const danmuCount = ref(0);
+const loadDanmuList = async() => {
+  if (!fileId.value) {
+    return []
+  }
+  let res = await proxy.request({
+    url: proxy.api.loadDanmu,
+    params: {
+      fileId: fileId.value,
+      videoId:route.params.videoId
+    }
+
+  })
+  if (!res) {
+    return
+  }
+  eventBus.emit('loadDanmu', res.data)
+  danmuCount.value = res.data.length
+  return res.data
+}
 
 </script>
 
 <style lang="scss" scoped>
-.player-panel{
-    .player-style{
-        width:100%;
-        :deep(.art-video-player .art-mask-show .art-state){
-            position: absolute;
-            right: 40px;
-            bottom: 50px;
+.player-panel {
+  .player-style {
+    width: 100%;
 
-            .art-icon-state{
-                width:60px;
-                height:60px;
-                img{
-                    width: 100%;
-                }
-            }
-        }
-        :deep(.art-controls-right){
-            position: relative;
-            display:block;
-            width: 280px;
-            
-            .art-control{
-                position: absolute;
-            }
-            //截屏
-            .art-control-screenshot{
-                left: 0px;
-            }
-            //设置按钮
-            .art-control-setting{
-                left: 46px;
-            }
-            //画中画
-            .art-control-pip{
-                left: 92px;
-            }
-            //宽屏
-            .art-control-wide-screen,.art-control-narrow-screen{
-                left: 138px;
-                .iconfont{
-                    font-size: 20px;
-                }
-            }
-            //网页全屏
-            .art-control-fullscreenWeb{
-                left: 184px;
-            }
-            //全屏按钮
-            .art-control-fullscreen{
-                left: 230px;
-            }
-        }
-    }
-    .danmu-panel{
-        box-shadow: 0 2px 6px #ddd;
-        height: 56px;
-        border-top: none;
-        background: #fff;
-        display: flex;
-        align-items: center;
-        padding: 0px 15px;
+    :deep(.art-video-player .art-mask-show .art-state) {
+      position: absolute;
+      right: 40px;
+      bottom: 50px;
 
-        .watcher {
-            width: 250px;
-            color: var(--text2);
-        }
-        #danmu{
-            flex:1;
-        }
+      .art-icon-state {
+        width: 60px;
+        height: 60px;
 
-        .close-danmu{
-            flex:1;
-            color: var(--text2);
-            text-align: center;
+        img {
+          width: 100%;
         }
+      }
     }
 
-    :deep(.danmu-setting){
-        background: none !important;
-        padding: 0px;
-        border: none;
-        .inner-panel{
-            width: 300px;
-            height: 280px;
-            position: relative;
+    :deep(.art-controls-right) {
+      position: relative;
+      display: block;
+      width: 280px;
 
-            .inner{
-                position: absolute;
-                left: 0px;
-                top: 0px;
-                z-index: 99999;
-                color: #fff;
-            }
-            .mask{
-                position: absolute;
-                top: 0px;
-                right: 0px;
-                left: 0px;
-                bottom: 0px;
-                background: #000;
-                opacity: 0.6;
-            }
+      .art-control {
+        position: absolute;
+      }
+
+      //截屏
+      .art-control-screenshot {
+        left: 0px;
+      }
+
+      //设置按钮
+      .art-control-setting {
+        left: 46px;
+      }
+
+      //画中画
+      .art-control-pip {
+        left: 92px;
+      }
+
+      //宽屏
+      .art-control-wide-screen,
+      .art-control-narrow-screen {
+        left: 138px;
+
+        .iconfont {
+          font-size: 20px;
         }
+      }
+
+      //网页全屏
+      .art-control-fullscreenWeb {
+        left: 184px;
+      }
+
+      //全屏按钮
+      .art-control-fullscreen {
+        left: 230px;
+      }
     }
+  }
+
+  .danmu-panel {
+    box-shadow: 0 2px 6px #ddd;
+    height: 56px;
+    border-top: none;
+    background: #fff;
+    display: flex;
+    align-items: center;
+    padding: 0px 15px;
+
+    .watcher {
+      width: 250px;
+      color: var(--text2);
+    }
+
+    #danmu {
+      flex: 1;
+    }
+
+    .close-danmu {
+      flex: 1;
+      color: var(--text2);
+      text-align: center;
+    }
+  }
+
+  :deep(.danmu-setting) {
+    background: none !important;
+    padding: 0px;
+    border: none;
+
+    .inner-panel {
+      width: 300px;
+      height: 280px;
+      position: relative;
+
+      .inner {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        z-index: 99999;
+        color: #fff;
+      }
+
+      .mask {
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        left: 0px;
+        bottom: 0px;
+        background: #000;
+        opacity: 0.6;
+      }
+    }
+  }
 }
 </style>
--- src/views/videodetail/index.vue
+++ src/views/videodetail/index.vue
@@ -3,52 +3,56 @@
         <div class="video-header">
             <div class="video-title">
                 <div class="title">{{ videoInfo.videoName }}</div>
-            </div>
-            <div class="video-info">
-                <div class="iconfont icon-play2">{{ videoInfo.playCount }}</div>
-                <div class="iconfont icon-danmu">{{ videoInfo.danmuCount }}</div>
+                <div class="video-info">
+                <div class="iconfont icon-play">{{ videoInfo.playCount }}</div>
+                <div class="iconfont icon-danmushu">{{ videoInfo.danmuCount }}</div>
                 <div class="iconfont">{{ videoInfo.createTime }}</div>
             </div>
+            </div>
+            
             <div class="video-user-info">
-            <Avatar :userId="userInfo.userId" :avatar="userInfo.avatar"></Avatar>
-            <div class="user-info">
-                <router-link class="nick-name" :to="`/user/${userInfo.userId}`" target="_blank">
-                    {{ userInfo.nickName }}
-                </router-link>
-                <div class="introduction">{{ userInfo.personIntroduction || '这个人很懒，什么都没有留下~' }}</div>
-                <div class="op-btns">
-                    <router-link class="btn-go-home" :to="`/user/${userInfo.userId}`" target="_blank">访问主页</router-link>
-                    <div class="focus-btn">
-                        <el-dropdown v-if="userInfo.haveFocus">
-                            <el-button class="btn" type="info" :style="{ width: '100%' }">
-                                <span class="iconfont icon-list"></span>
-                                已关注
-                                {{ userInfo.fansCount }}
+                <Avatar :userId="userInfo.userId" :avatar="userInfo.avatar"></Avatar>
+                <div class="user-info">
+                    <router-link class="nick-name" :to="`/user/${userInfo.userId}`" target="_blank">
+                        {{ userInfo.nickName
+                        }}
+                    </router-link>
+                    <div class="introduction">{{ userInfo.personIntroduction || '这个人很懒，什么都没有留下~' }}</div>
+                    <div class="op-btns">
+                        <router-link class="btn-go-home" :to="`/user/${userInfo.userId}`"
+                            target="_blank">访问主页</router-link>
+                        <div class="focus-btn">
+                            <el-dropdown v-if="userInfo.haveFocus">
+                                <el-button class="btn" type="info" :style="{ width: '100%' }">
+                                    <span class="iconfont icon-list"></span>
+                                    已关注
+                                    {{ userInfo.fansCount }}
+                                </el-button>
+                                <template #dropdown>
+                                    <el-dropdown-menu>
+                                        <el-dropdown-item @click="focusUser(-1)">取消关注</el-dropdown-item>
+                                    </el-dropdown-menu>
+                                </template>
+                            </el-dropdown>
+                            <el-button class="btn" type="primary" :style="{ width: '100%' }" @click="focusUser(1)"
+                                v-else>
+                                关注{{ userInfo.fansCount
+                                }}
                             </el-button>
-                            <template #dropdown>
-                                <el-dropdown-menu>
-                                    <el-dropdown-item @click="focusUser(-1)">取消关注</el-dropdown-item>
-                                </el-dropdown-menu>
-                            </template>
-                        </el-dropdown>
-                        <el-button class="btn" type="primary" :style="{ width: '100%' }" @click="focusUser(1)" v-else>
-                            关注{{ userInfo.fansCount }}
-                        </el-button>
+                        </div>
                     </div>
                 </div>
             </div>
-            
-        </div>
         </div>
         <div class="video-body">
             <div class="video-left">
-                <div class="video-panel">
-                    <Player ref="playerRef"></Player>
+                <div class="video-panel" :style="{ position: wideScreen ? 'absolute' : 'static' }">
+                    <Player ref="playerRef" @changeWideScreen="changeWideScreenHandler"></Player>
                 </div>
             </div>
+            <div class="video-right"></div>
         </div>
     </div>
-    
 </template>
 
 <script setup>
@@ -84,7 +88,7 @@ const getUserInfo = async userId => {
     userInfo.value = res.data;
 };
 
-const focusUser = async (changeCount) => {
+const focusUser = async changeCount => {
     // 优先使用 Pinia 中的登录信息（由登录流程或 autoLogin 填充），
     // 若不存在再回退到 localStorage（兼容旧逻辑）。
     let isLogged = false;
@@ -98,7 +102,7 @@ const focusUser = async (changeCount) => {
     }
     if (!isLogged) {
         try {
-            const raw = localStorage.getItem('userInfo');
+            const raw = localStorage.getItem("userInfo");
             const storedUser = raw ? JSON.parse(raw) : null;
             if (storedUser && Object.keys(storedUser).length > 0) {
                 isLogged = true;
@@ -117,18 +121,18 @@ const focusUser = async (changeCount) => {
         params: {
             focusUserId: userInfo.value.userId
         }
-    })
-    if(!res){
+    });
+    if (!res) {
         return;
     }
-    if(changeCount == 1){
+    if (changeCount == 1) {
         userInfo.value.haveFocus = true;
         userInfo.value.fansCount += 1;
-    }else{
+    } else {
         userInfo.value.haveFocus = false;
         userInfo.value.fansCount -= 1;
     }
-}
+};
 
 const videoInfo = ref({});
 const getVideoInfo = async () => {
@@ -156,6 +160,12 @@ onMounted(() => {
         navActionStore.setForceFixedHeader(true);
     });
 });
+
+const wideScreen = ref(false);
+const changeWideScreenHandler = (result) => {
+    wideScreen.value = result;
+};
+
 </script>
 
 <style lang="scss" scoped>
@@ -172,25 +182,29 @@ onMounted(() => {
 
             .title {
                 font-size: 22px;
+                color: var(--text);
             }
-
             .video-info {
                 margin-top: 10px;
                 display: flex;
                 color: #9499a0;
-
+                align-items: center;
+                
                 .iconfont {
                     margin-right: 20px;
                     font-size: 14px;
-
+                    line-height:24px;
+                    
                     &::before {
                         font-size: 18px;
                         margin-right: 3px;
                     }
                 }
             }
-        }
 
+            
+        }
+        
         .video-user-info {
             margin-left: 20px;
             width: 410px;
@@ -258,7 +272,7 @@ onMounted(() => {
     }
 
     .video-body {
-        margin-top: 20px;
+        margin-top: 15px;
         position: relative;
         display: flex;
 
